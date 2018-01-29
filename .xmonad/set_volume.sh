bae2845b2f962980ba060d5d5d400eb3311702dd
Orestis Melkonian <melkon.or@gmail.com>
2018-01-29 11:45:52 +0100

    bash: aliases, xmonad: audio fix, subl: theme

--- .xmonad/set_volume.sh
+++ .xmonad/set_volume.sh
@@ -2,15 +2,14 @@
 
 external='Saffire'
 internal='Built-in'
-external_id='alsa_output.usb-Focusrite_Audio_Engineering_Saffire_6USB-00.analog-surround-40'
+external_id='alsa_output.usb-Focusrite'
 if [ "$(hostname)" == "omelkonian-XPS-15-9560" ]; then # Dell
     internal_id='alsa_output.pci-0000_00_1f.3.analog-stereo'
 else # HP
     internal_id='alsa_output.pci-0000_00_1b.0.analog-stereo'
 fi
-
 if [ -n "$(pactl list sinks | grep $external)" ]; then
-    card=$external_id
+    card=$(pactl list sinks | grep "Name: $external_id" -B 2 | grep 'Sink #' | grep -o -E '[[:digit:]]+')
 else
     card=$internal_id
 fi
