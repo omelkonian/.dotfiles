7b1c694a8383f3416c36b772145006f6ea2c0305
Orestis Melkonian <melkon.or@gmail.com>
2019-10-13 22:43:58 +0100

    xmonad: update audio stereo checking

--- .xmonad/scripts/get_vpn.sh
+++ .xmonad/scripts/get_vpn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 case $(nordvpn status | head -n1) in
   "Status: Connected")
-    echo "<action=nordvpn d>∃</action>";;
+    echo "<action=nordvpn d><fc=green>∃</fc></action>";;
   *)
-    echo "<action=nordvpn c nl492>∄</action>";;
+    echo "<action=nordvpn c nl492><fc=red>∄</fc></action>";;
 esac
