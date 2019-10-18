21c742585b43a27a91612e3f86e8461ec1aaebf0
Orestis Melkonian <melkon.or@gmail.com>
2019-10-18 09:47:56 +0100

    cronjob: more frequent vpn checks

--- cronjobs/vpn_check.sh
+++ cronjobs/vpn_check.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-pid=$(pgrep transmission)
-vpnStatus=$(nordvpn status | grep "Disconnected")
-if [ -n "$pid" ] && [ -n "$vpnStatus" ]; then
-  kill -9 $pid
-fi
+for i in {1..59}; do
+  pid=$(pgrep transmission)
+  vpnStatus=$(nordvpn status | grep "Disconnected")
+  if [ -n "$pid" ] && [ -n "$vpnStatus" ]; then
+    notify-send "NO VPN: Killing transmission"
+    kill -9 $pid
+  fi
+  sleep 1;
+done
