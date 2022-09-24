b3598dbbcb4462290310e2c002953ab28e4c7f6d
Orestis Melkonian <melkon.or@gmail.com>
2022-09-24 21:27:20 +0100

    VPN: add proton alternative

--- .xmonad/scripts/get_vpn.sh
+++ .xmonad/scripts/get_vpn.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-if [ -n "$(nordvpn status | grep 'Status: Connected')" ] ; then
-    echo "<action=nordvpn d><fc=green>∃</fc></action>"
+
+nord__connect='nordvpn c P2P'
+nord__disconnect='nordvpn d'
+proton__connect='protonvpn-cli c -p udp --fastest'
+proton__disconnect='protonvpn-cli d'
+
+if [ -n "$(nordvpn status | grep 'Status: Connected')" ]; then
+    echo "<action=$nord__disconnect><fc=green>∃</fc></action>"
+elif [ -n "$(protonvpn-cli s | grep 'Connection time:')" ]; then
+    echo "<action=$proton__disconnect><fc=green>∃</fc></action>"
 else
-    echo "<action=nordvpn c P2P><fc=red>∄</fc></action>"
+    echo "<action=$proton__connect><fc=red>∄</fc></action>"
 fi
