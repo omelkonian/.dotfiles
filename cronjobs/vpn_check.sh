3cad1d956d382e901faf0449f8746b8e5cf43e32
Orestis Melkonian <melkon.or@gmail.com>
2022-11-04 09:24:45 +0000

    minor tweaks

--- cronjobs/vpn_check.sh
+++ cronjobs/vpn_check.sh
@@ -4,7 +4,7 @@ for i in {1..59}; do
   vpnStatus=$(nordvpn status | grep "Disconnected")
   if [ -n "$pid" ] && [ -n "$vpnStatus" ]; then
     notify-send "NO VPN: Killing transmission"
-    kill -9 $pid
+    # kill -9 $pid
   fi
   sleep 1;
 done
