d15fb7dd0d36ee530fef885d94cb358bc77fcee3
Orestis Melkonian <melkon.or@gmail.com>
2024-02-29 19:46:36 +0000

    bash: update xmobar

--- .xmonad/scripts/video_bar.sh
+++ .xmonad/scripts/video_bar.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 source /home/omelkonian/git/.dotfiles/bash/video.symlink
+source /home/omelkonian/git/.dotfiles/bash/keyboard.symlink
+source /home/omelkonian/git/.dotfiles/bash/settings.symlink
+
+# DnD
+color=$($(dnd__enabled) && echo 'green' || echo 'red')
+action='callFun.sh /home/omelkonian/git/.dotfiles/bash/settings.symlink dnd__toggle'
+echo -n "<action=${action}><fc=${color}><icon=alert.xbm/></fc></action>"
+
+# Remote touchpad
+color=$($(remote-touchpad__enabled) && echo 'green' || echo 'red')
+action='callFun.sh /home/omelkonian/git/.dotfiles/bash/keyboard.symlink remote-touchpad__toggle'
+echo -n "<action=${action}><fc=${color}><icon=stop.xbm/></fc></action>"
 
 # Rotate screen
-echo -n "<action=callFun.sh /home/omelkonian/git/.dotfiles/bash/video.symlink rotateScreen>\
+cmd="zenity --question --text 'Are you sure you want to rotate the screen?'"
+echo -n "<action=$cmd && callFun.sh /home/omelkonian/git/.dotfiles/bash/video.symlink rotateScreen>\
 <icon=repeat.xbm/></action> "
 
 # GPUs
@@ -11,4 +24,3 @@ fc2=$([[ $(echo `prime-select query`) == "$GPU2" ]] && echo 'green' || echo 'red
 cmd1="zenity --password | sudo -S prime-select $GPU1"
 cmd2="zenity --password | sudo -S prime-select $GPU2"
 echo "<action=$cmd1><fc=$fc1>intel</fc></action>/<action=$cmd2><fc=$fc2>nvidia</fc></action>"
-
